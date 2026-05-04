88f387cf94b9f5fdcdabdf3d7160e22c9002861d
Kyle <kyjaychristensen@gmail.com>
2026-05-04 10:59:30 -0600

    Class 1 folder created

---
+++ Class 1/predictions.py
@@ -0,0 +1,33 @@
+x = 10
+y = 3
+print (x / y)
+
+
+x = 10
+y = 3
+print (x // y)
+
+
+name = "Python"
+print(name * 3)
+
+
+val = input("Num: ")
+print(int(val))
+
+
+a = "5"
+b = "3"
+print(a + b)
+
+
+price = 19.99
+qty = 3
+total = price * qty
+print (f"${total: .2f}")
+
+
+item = "fruit"
+price = 4.50
+print(f"One {item}"
+    f" costs ${price}")
